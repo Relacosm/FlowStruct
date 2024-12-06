39c8426b50727eae9a4ce66fd6cb32ba174f3429
Relacosm <hraj51399@gmail.com>
2024-12-06 18:18:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -383,6 +383,7 @@ const CodeFlowVisualizer = () => {
     <div className="code-flow-container">
       <div className="header">
         <h1>FlowStruct</h1>
+        <h5>For optimal viewing on mobile, switch to desktop mode.</h5>
         <p>Transform your code into an interactive visual journey across multiple languages</p>
       </div>
       
